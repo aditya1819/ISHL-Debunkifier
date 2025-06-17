af4a57b1541d32fe993b4ca90d117620e4aad521
Aditya Shahare <shahare.aditya@gmail.com>
2025-06-17 16:14:13 +0200

    feat: question model

---
+++ app/Models/Question.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'image',
+        'possible_reasons',
+        'section_count',
+        'section_data'
+    ];
+
+    protected $casts = [
+        'possible_reasons' => 'array',
+        'section_data' => 'array'
+    ];
+
+    // Accessor to get image URL
+    public function getImageUrlAttribute()
+    {
+        return $this->image ? asset('storage/questions/' . $this->image) : null;
+    }
+}
